c3d0bb8f2445b834cc26c6492488dbd1f0184d2b
John Else <john.else@citrix.com>
2014-09-09 12:31:03 +0100

    xapi_vdi: Introduce VDI operations needed for revert
    This is for both the live VDI (revert_from) and the snapshot (revert_to)
    
    These correspond to the VM operations `reverting` and `revert`,
    respectively.
    
    Only snapshots backed by an SR that allows clones are allowed to be
    reverted to. This is because SRs supporting revert is not enough to use
    vdi revert without clone: having an incorrect snapshot_of field will
    also cause the use of clone. With this we can ensure that if a bad edge
    case is met the system has a working fallback.
    
    Reverting to "live" snapshot VDIs is allowed, this happens when
    reverting to a checkpoint.
    
    Currently reverting requires the cloning feature because it's not
    possible to discriminate between backends that support vdi revert and
    ones that done. This will be possible once all of them do
    
    Test that VDIs...
    * can be reverted to a snapshot.
    * can be reverted to an attached snapshot.
    * cannot be reverted to a leaf VDI.
    * cannot be reverted to an attached leaf.
    
    Co-authored-by: John Else <john.else@citrix.com>
    Signed-off-by: Pau Ruiz Safont <pau.safont@vates.tech>

--- ocaml/idl/schematest.ml
+++ ocaml/idl/schematest.ml
@@ -3,7 +3,7 @@ let hash x = Digest.string x |> Digest.to_hex
 (* BEWARE: if this changes, check that schema has been bumped accordingly in
    ocaml/idl/datamodel_common.ml, usually schema_minor_vsn *)
 
-let last_known_schema_hash = "88b40556fd6a45af918900ff6d8079c5"
+let last_known_schema_hash = "23c1d4d13c144daa5dade4bc1d5f6c71"
 
 let current_schema_hash : string =
   let open Datamodel_types in
