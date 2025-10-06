e96d858a3b7a5ee0a258d6c0d8a14800d23fb8ea
Pau Ruiz Safont <pau.safont@vates.tech>
2025-10-06 11:15:11 +0100

    xapi_vm_snapshot: change VM.revert to use VDI.revert
    The code now tries to call VDI.revert on all the disk VDIs, and if that
    fails, it falls back to the original revert method that destroys VM
    disks, clones snapshot disks and then fixes up metadata.
    
    This means that the successfully reverted disks must be excluded from
    the original method. This is done by introducing sets of VDIs and VBDs
    and using set logic on them.
    
    The CD disks and the suspend VDI are treated like before: destroy +
    clone.
    
    The code is more convoluted that I would have liked because of existing
    clone infrastructure mixes VBDs with VDIs, so they need to be converted
    back and forth to be able to run the previous method correctly,
    especially for updating the snapshot_of field of the cloned snapshot
    disks.
    
    Quicktests encodes the difference in behaviour from the original method
    to the native one: now VDI UUIDs are not changed when reverting.
    
    Signed-off-by: Pau Ruiz Safont <pau.safont@vates.tech>

--- ocaml/idl/schematest.ml
+++ ocaml/idl/schematest.ml
@@ -3,7 +3,7 @@ let hash x = Digest.string x |> Digest.to_hex
 (* BEWARE: if this changes, check that schema has been bumped accordingly in
    ocaml/idl/datamodel_common.ml, usually schema_minor_vsn *)
 
-let last_known_schema_hash = "ba70ce2024e319b7e3969f9c1d3b0c15"
+let last_known_schema_hash = "9b33f67f55f2add144fabd7af33bcb55"
 
 let current_schema_hash : string =
   let open Datamodel_types in
