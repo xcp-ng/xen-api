6d76018c8804036732fda75e6cf094f3d141abac
Sebastien Rodot <sebastien.rodot@vates.tech>
2026-03-25 10:16:26 +0100

    VLAN filtering on VIF
    - update idl/datamodel to add trunks property on VIF
    - add validation constraints (trunks filtering on access port isn't valid)
    - add support inside `vif-real` for initial configuration
    - add support inside xenopsd for dynamic changes
    - add test coverage to new attributes
    
    References:
    
    - Design document: https://github.com/xapi-project/xen-api/blob/788869e5a92e10332ba2428eb91e5a2caf4c7131/doc/content/design/vlan-filtering.md
    
    Signed-off-by: Sebastien Rodot <sebastien.rodot@vates.tech>
    (cherry picked from commit 68c50c38a87d0addc743f2ab93b3b1d032033a6b + conflicts resolution)

--- ocaml/idl/datamodel_lifecycle.ml
+++ ocaml/idl/datamodel_lifecycle.ml
@@ -99,6 +99,8 @@ let prototyped_of_field = function
       Some "22.26.0"
   | "SM", "host_pending_features" ->
       Some "24.37.0"
+  | "VIF", "trunks" ->
+      Some "26.1.13-next"
   | "host", "timezone" ->
       Some "26.0.0"
   | "host", "ntp_custom_servers" ->
@@ -243,6 +245,12 @@ let prototyped_of_message = function
       Some "22.26.0"
   | "VDI", "revert" ->
       Some "26.1.4-next"
+  | "VIF", "set_trunks" ->
+      Some "26.1.13-next"
+  | "VIF", "remove_trunks" ->
+      Some "26.1.13-next"
+  | "VIF", "add_trunks" ->
+      Some "26.1.13-next"
   | "host", "set_servertime" ->
       Some "26.0.0"
   | "host", "get_ntp_synchronized" ->
